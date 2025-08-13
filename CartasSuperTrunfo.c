e85498e1399e566357579e4ea39c1ccbf83063a0
Matheus Reis <matheusreis.odesigner@gmail.com>
2025-08-13 22:45:15 +0000

    Declarando variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,13 +12,17 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     int código;
     char nome[20];
-    
-
+    int população;
+    float área;
+    float PIB;
+    int pontos;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("Digite o código da cidade: \n")
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
