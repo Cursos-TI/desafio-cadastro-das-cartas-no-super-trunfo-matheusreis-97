8970f114962c5a462a3722d4e78509046623b163
Matheus Reis <matheusreis.odesigner@gmail.com>
2025-08-14 01:15:35 +0000

    PRIMEIRA CARTA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,26 +22,41 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     printf("Digite o código da cidade: \n");
-    sacnf(" %d", &codigo);
-
+    scanf("%d", &codigo);
+    
     printf("Digite o nome da cidade: \n");
     scanf("%s", &nome);
-
+    
     printf("Digite a população da cidade: \n");
     scanf("%d", &populacao);
-
-    printf("Digite a área (em km²)");
+    
+    printf("Digite a área (em km²): \n");
     scanf("%f", &area);
-
+    
     printf("Digite o PIB: \n");
     scanf("%f", &pib);
-
+   
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontos);
+    
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+     printf("O código da cidade é: %d\n", codigo);
+
+     printf("O nome da cidade é: %s\n", nome);
+
+     printf("A população da cidade é de: %d\n", populacao);
+
+     printf("A área da cidade é de: %f\n", area);
+
+     printf("O PIB da cidade é: %f\n", pib);
+
+     printf("A cidade tem: %d pontos turísticos.\n", pontos);
+
+
 
     return 0;
 }
